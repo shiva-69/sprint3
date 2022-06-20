538ea79a87eff4a02ce8cd584fcd5d40adaf450d
shiva-69 <92380211+shiva-69@users.noreply.github.com>
2022-06-20 22:00:25 +0530

    final

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,35 @@ import React from "react";
 import { CartContext } from "../context/CartContext";
 
 const Home = () => {
-  const {cartCount} = React.useContext(CartContext);
+  const [Loading, setLoading] = React.useState(true)
+
+  const [list, setList] = React.useState([]);
+  const {cartCount, handleCartCount} = React.useContext(CartContext);
+
+  React.useEffect(()=>{
+    getData();
+  },[]);
+
+  const getData =() => {
+    fetch(`http://localhost:8080/products`)
+    .then(res => res.json())
+    .then(res => setList(res))
+    .finally(()=>setLoading(false));
+  }
+  const handleChange= (val) => {
+    handleCartCount(val);
+  }
   return <div>
-    {cartCount}
+    {Loading ? <h1>Loading....</h1> : list.map((item) => (
+    <div key={item.id}>
+      <h3>{item.name}</h3>
+      <h4>{item.description}</h4>
+      <button onClick={()=>handleChange(+1)}>Increase Quantity</button>
+      <button onClick={()=>handleChange(-1)}>Decrease Quantity</button>
+    </div>
+    ))
+    
+    }
 
   </div>;
 };
